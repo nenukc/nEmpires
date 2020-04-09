0c4e1da240aa3de9b0c172700cde93b4dec72d2c
india1a <chaitanya.marella@gmail.com>
2020-04-09 16:08:19 +0530

    Checking git commands - status and log

--- src/nEmpires/nEmpires.java
+++ src/nEmpires/nEmpires.java
@@ -148,6 +148,7 @@ public class nEmpires
         placeEmperors();
         System.out.println("Total of "+q+" Emperors can be placed on a board with "+(n*n)+" Squares");
         /*Change 1 on Laptop*/
+        /*Checking git status and git log commands*/
       
     }
         
